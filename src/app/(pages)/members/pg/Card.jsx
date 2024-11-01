6cbedb788341e24e68a44f9836c928b752340819
HarshN12 <hneema12@gmail.com>
2024-11-01 15:07:52 +0530

    home page updated

--- src/app/(pages)/members/pg/Card.jsx
+++ src/app/(pages)/members/pg/Card.jsx
@@ -5,12 +5,13 @@ const PlaceholderImg = '/assets/unknownman.png';
 const Lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?';
 
 
-function Card({Name='Name',CardImg= PlaceholderImg ,Descrip=Lorem}){
+function Card({Name='Name',CardImg= PlaceholderImg ,Descrip=Lorem,}){
     return(
     <div className = {styles['box']}>
       <img src={CardImg} alt="profile_img"></img>
       <h3>{Name}</h3>
       <p>{Descrip}</p>
+     
     </div>
     )
 }
--- src/app/(pages)/members/pg/Container.jsx
+++ src/app/(pages)/members/pg/Container.jsx
@@ -1,4 +1,3 @@
-
 import Card from './Card';
 import styles from './Container.module.css'; 
 
